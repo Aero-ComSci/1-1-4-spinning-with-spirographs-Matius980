6f1342377b15dcd972640cb61e178af46b555ed1
Matius980 <157426874+Matius980@users.noreply.github.com>
2024-09-12 22:17:35 -0700

    Update Problem2.py
    Import turtle, Create class with function to draw dot

--- Problem2.py
+++ Problem2.py
@@ -1,1 +1,8 @@
+import turtle
 
+# Function to draw a black dot at a specific position
+def draw_object(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.dot(50, "blue")  # Draws a black circle with a radius of 50
